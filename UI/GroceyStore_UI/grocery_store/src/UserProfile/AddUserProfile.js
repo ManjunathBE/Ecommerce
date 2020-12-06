732f51a8b73f531714e261eca3377cd671eddca1
Sharique Maaz <sharique.maaz.ext@sivantos.com>
2020-12-07 01:57:58 +0530

    added basic validations

--- UI/GroceyStore_UI/grocery_store/src/UserProfile/AddUserProfile.js
+++ UI/GroceyStore_UI/grocery_store/src/UserProfile/AddUserProfile.js
@@ -10,8 +10,10 @@ import { useStore } from "../Store"
 export const AddUserProfile = (props) => {
     const { history } = props
 
-    var user = { address: [{ addressLine1: "", addressLine2: "", city: "", pinCode: "" }], firstName: "", lastName: "", email: "", phoneNumber: "" }
+    let user = { address: [{ addressLine1: "", addressLine2: "", city: "", pinCode: "" }], firstName: "", lastName: "", email: "", phoneNumber: "" }
+    const [userDetails, setUserDetails] = useState(user)
     const { userStore, setUserStore } = useStore()
+    const [errors, setErrors] = useState({})
 
 
     const address = () => {
@@ -24,6 +26,8 @@ export const AddUserProfile = (props) => {
                     label="Address Line 1"
                     name="addressLine1"
                     onChange={handleAddressChange}
+                    error={errors.addressLine1}
+                    helperText={errors.addressLine1}
                 />
                 <br />
                 <TextField
@@ -42,6 +46,8 @@ export const AddUserProfile = (props) => {
                     label="City"
                     name="city"
                     onChange={handleAddressChange}
+                    error={errors.city}
+                    helperText={errors.city}
 
                 />
                 <br />
@@ -53,30 +59,61 @@ export const AddUserProfile = (props) => {
                     name="pinCode"
                     type="number"
                     onChange={handleAddressChange}
+                    error={errors.pinCode}
+                    helperText={errors.pinCode}
 
                 />
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+
             </div>
         )
     }
 
     const handleAddressChange = (event) => {
         var { name, value } = event.target
-        user.address.map((x) => x.[name] = value)
+        userDetails.address.map((x) => x.[name] = value)
 
     }
     const handleFormChange = (event) => {
+        console.log(userDetails, 'user state')
+        console.log('inchange value', event.target.value)
+        console.log('inchange event for ',event.target.name)
         var value = event.target.value
         if (event.target.name === 'phoneNumber') {
             value = Number(value)
         }
-        user.[event.target.name] = value
+        userDetails.[event.target.name] = value
         //setUser()
     }
 
-    const handleSubmit = (event) => {
+    const validate = (event)=>{
+        const temp =[]
+        temp.firstName = userDetails.firstName? "" : "First name is required"
+        temp.email = userDetails.email ? "" :"Email is required"
 
+        temp.phone = userDetails.phoneNumber ? "" : "Phone is required"
+        if (!temp.phone) temp.phone = userDetails.phoneNumber.toString().length == 10 ? "" : "phone number should be 10 digits"
+
+    userDetails.address.forEach(address => {
+        temp.addressLine1 =  address.addressLine1? "" : "Address Line 1 is required"
+           temp.city = address.city? "" : "City is required"      
+           temp.pinCode = address.pinCode? "" : "pin code is required"
+           if(!temp.pinCode) temp.pinCode = address.pinCode.length === 6 ?"":"Pincode should be 6 digits long"
+            
+    });
+
+        setErrors({ ...temp })
+        return Object.values(temp).every(param => param === "")
+    }
+
+    const handleSubmit = (event) => {
+        console.log('in submit', userDetails)
         event.preventDefault()
-        console.log(user)
+        if(validate()){
+        console.log(user,'user in submit')
 
 
         fetch('https://localhost:44360/api/user', {
@@ -84,7 +121,7 @@ export const AddUserProfile = (props) => {
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(user)
+            body: JSON.stringify(userDetails)
         })
             .then(result => {
                 if (result.status === 201) {
@@ -95,25 +132,29 @@ export const AddUserProfile = (props) => {
                     history.push('/')
                 }
             })
+        }
     }
 
     return (
         <div>
             <Header title={(props.location.pathname).substring(1)} history={props.history} />
             <form >
+            <Button onClick={handleSubmit}>Update</Button>
                 <TextField
                     margin="normal"
                     required
                     id="FirstName"
-                    label="FirstName"
+                    label="First Name"
                     name="firstName"
                     autoFocus
                     onChange={handleFormChange}
+                    error={errors.firstName}
+                    helperText={errors.firstName}
                 />
                 <TextField
                     margin="normal"
                     required
-                    id="LastName"
+                    id="Last Name"
                     label="LastName"
                     name="lastName"
                     onChange={handleFormChange}
@@ -124,19 +165,24 @@ export const AddUserProfile = (props) => {
                     id="Email"
                     label="Email"
                     name="email"
+                    type="email"
                     onChange={handleFormChange}
+                    error={errors.email}
+                    helperText={errors.email}
                 />
                 <TextField
                     margin="normal"
                     required
-                    id="Phone"
+                    id="Phone (10 digits)"
                     label="Phone Number"
                     name="phoneNumber"
                     onChange={handleFormChange}
+                    error={errors.phone}
+                    helperText={errors.phone}
                 />
             </form>
             {address()}
-            <Button onClick={handleSubmit}>Update</Button>
+            
 
         </div>
     )
