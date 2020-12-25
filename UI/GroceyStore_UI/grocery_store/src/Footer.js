441101a87024805043a3248db329706b7d1decc3
Sharique Maaz <sharique.maaz.ext@sivantos.com>
2020-12-26 03:35:14 +0530

    refactored

--- UI/GroceyStore_UI/grocery_store/src/Footer.js
+++ UI/GroceyStore_UI/grocery_store/src/Footer.js
@@ -1,22 +1,26 @@
 import React, { Component } from "react"
 import { Router, Link } from 'react-router-dom'
 
-class Footer extends Component {
-    constructor(props) {
-        super(props);
+
+export const Footer=(props)=>{
+    const {history} = props
+
+    const getContent=()=>{
+        
+        if(window.location.pathname==='/login') return null
+        else{
+            return ( <div id="footer">
+            <Link className="footerLink icon col-2" to="/UserProfile"> User Profile</Link>
+            <Link className="footerLink icon col-2" to="/History"> History</Link>
+            <Link className="footerLink icon col-2" to="/Cart"> Cart</Link>
+            <Link className="footerLink icon col-3" to="/Select View"> Select View</Link>
+            <Link className="footerLink icon col-3" to="/Notification"> Notifaction</Link>
+        </div>)
+        }
+    }
+        
+return getContent()
     }
 
-    render() {
-        return (
-            <div id="footer">
-                <Link className="footerLink icon col-2" to="/UserProfile"> User Profile</Link>
-                <Link className="footerLink icon col-2" to="/History"> History</Link>
-                <Link className="footerLink icon col-2" to="/Cart"> Cart</Link>
-                <Link className="footerLink icon col-3" to="/Select View"> Select View</Link>
-                <Link className="footerLink icon col-3" to="/Notification"> Notifaction</Link>
-            </div>
-        )
-    }
-}
 
 export default Footer
