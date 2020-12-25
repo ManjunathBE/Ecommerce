f7d9fc49d6db9df5ab18138f20629dee62db3653
Sharique Maaz <sharique.maaz.ext@sivantos.com>
2020-12-26 03:35:57 +0530

    conditional visibility of footer

--- UI/GroceyStore_UI/grocery_store/src/App.js
+++ UI/GroceyStore_UI/grocery_store/src/App.js
@@ -1,21 +1,27 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { Routes } from './Routes'
 import { AuthContext } from "./Auth";
 import Footer from './Footer'
+import { Hidden } from '@material-ui/core';
+import {MenuPane} from './MenuPane'
 
 
-function App() {
+
+
+function App() { 
+
   const [loginState, setLoginState] = useState(false)
   console.log(loginState, 'login state')
   return (
-    <AuthContext.Provider value={{loginState, setLoginState}}>
-     {/* <AuthContext.Provider value={true}> */}
-    <div >
-
-      <Routes />
-      <Footer />
-    </div>
+    <AuthContext.Provider value={{ loginState, setLoginState }}>
+      {/* <AuthContext.Provider value={true}> */}
+      <div >
+        <Routes />
+        <Hidden mdUp >
+          <Footer />
+        </Hidden>
+      </div>
     </AuthContext.Provider>
   );
 }
