162873e149f150b3c27d84e8babe890229a3ae8d
Sharique Maaz <sharique.maaz.ext@sivantos.com>
2021-01-20 02:01:55 +0530

    firebase config file

---
+++ UI/GroceyStore_UI/grocery_store/src/firebaseConfig.js
@@ -0,0 +1,21 @@
+
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+{/* <script src="https://www.gstatic.com/firebasejs/8.2.1/firebase-app.js"></script> */}
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+
+{/* <script>
+  // Your web app's Firebase configuration */}
+  const firebaseConfig = {
+    apiKey: "AIzaSyAtKVBsuK6i2UpdBHzxxwQ4tAovaYrjwdg",
+    authDomain: "grocerystoreauth.firebaseapp.com",
+    projectId: "grocerystoreauth",
+    storageBucket: "grocerystoreauth.appspot.com",
+    messagingSenderId: "206463662716",
+    appId: "1:206463662716:web:a75e05bfc8f0ee325227f9"
+  };
+  // Initialize Firebase
+// </script>
+
+export default firebaseConfig;
