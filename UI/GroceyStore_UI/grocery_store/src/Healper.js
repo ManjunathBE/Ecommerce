65f88a2ba46eccae1f5e0211f4bd24767cbe65e3
Sharique Maaz <sharique.maaz.ext@sivantos.com>
2021-01-20 02:02:55 +0530

    added unit mapper helper method

--- UI/GroceyStore_UI/grocery_store/src/Healper.js
+++ UI/GroceyStore_UI/grocery_store/src/Healper.js
@@ -1,2 +1,7 @@
 export const toFirstCharUppercase = (name) =>
   name.charAt(0).toUpperCase() + name.slice(1);
+
+export const unitMapper = (unitId) =>{
+  if(unitId===1) return 'Kg'
+  else return 'Numbers'
+}
