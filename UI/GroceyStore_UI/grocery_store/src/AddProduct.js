bcf9894a31ccf97f355d570df28445e9a5400736
Sharique Maaz <sharique.maaz.ext@sivantos.com>
2021-04-07 19:25:53 +0530

    extended kg values to 50

--- UI/GroceyStore_UI/grocery_store/src/AddProduct.js
+++ UI/GroceyStore_UI/grocery_store/src/AddProduct.js
@@ -43,21 +43,16 @@ export const AddProduct = (props) => {
 
 
 
+    var kgOptions = [];
+    for (var i = 0; i <= 50; i++) {
+        kgOptions.push({ value: i, label: i })
+    }
+
     const [valueGroupsKG, setValueGroupsKG] = useState({
         Weight: 0
     })
     const [optionGroupsKG, setOptionGroupsKG] = useState({
-        Weight: [
-            { value: 0, label: '0' },
-            { value: 1, label: '1' },
-            { value: 2, label: '2' },
-            { value: 3, label: '3' },
-            { value: 4, label: '4' },
-            { value: 5, label: '5' },
-            { value: 6, label: '6' },
-            { value: 7, label: '7' },
-            { value: 8, label: '8' },
-        ],
+        Weight: kgOptions
     })
     const [valueGroupsGrams, setValueGroupsGrams] = useState({
         Weight: 0
